75fdf61ba918bb2cfe517bfb5f531d9c81b4977a
Andrew MacGinitie <macginitie@charter.net>
2018-09-21 18:59:18 -0400

    getting closer to spec

--- WpfTextRender/OutlineText.cs
+++ WpfTextRender/OutlineText.cs
@@ -16,35 +16,6 @@ using System.Windows.Shapes;
 
 namespace WpfTextRender
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:WpfTextRender"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:WpfTextRender;assembly=WpfTextRender"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:OutlineText/>
-    ///
-    /// </summary>
     public class OutlineText : Control
     {
         static OutlineText()
@@ -59,7 +30,7 @@ namespace WpfTextRender
         /// Create the outline geometry based on the formatted text.
         /// </summary>
         public static void CreateText(string Text, FontFamily Font = null, bool Bold = false, bool Italic = false,
-                                      double FontSize = 240.0, bool Highlight = false)
+                                      double FontSize = 359.0, bool Highlight = false)
         {
             System.Windows.FontStyle fontStyle = FontStyles.Normal;
             FontWeight fontWeight = FontWeights.Medium;
@@ -68,22 +39,27 @@ namespace WpfTextRender
             if (Italic == true) fontStyle = FontStyles.Italic;
             if (null == Font) Font = new FontFamily("Arial");
 
-            // Create the formatted text based on the properties set.
-            FormattedText formattedText = new FormattedText(
-                Text,
-                CultureInfo.GetCultureInfo("en-us"),
-                FlowDirection.LeftToRight,
-                new Typeface(
-                    Font,
-                    fontStyle,
-                    fontWeight,
-                    FontStretches.Normal),
-                FontSize,
-                System.Windows.Media.Brushes.Black // This brush does not matter since we use the geometry of the text. 
-                );
+            // this loop brute forces the size (i.e, the conversion from FontSize to 300px height)
+            do
+            {
+                // Create the formatted text based on the properties set.
+                FormattedText formattedText = new FormattedText(
+                    Text,
+                    CultureInfo.GetCultureInfo("en-us"),
+                    FlowDirection.LeftToRight,
+                    new Typeface(
+                        Font,
+                        fontStyle,
+                        fontWeight,
+                        FontStretches.Normal),
+                    FontSize,
+                    System.Windows.Media.Brushes.Black // This brush does not matter since we use the geometry of the text. 
+                    );
 
-            // Build the geometry object that represents the text.
-            _textGeometry = formattedText.BuildGeometry(new System.Windows.Point(0, 0));
+                // Build the geometry object that represents the text.
+                _textGeometry = formattedText.BuildGeometry(new System.Windows.Point(0, 0));
+                FontSize += 0.1;
+            } while (_textGeometry.Bounds.Bottom - _textGeometry.Bounds.Top < 260); // magic # = 300 - 20*2 [stroke width top+bottom]
         }
 
         /// <summary>
@@ -92,14 +68,14 @@ namespace WpfTextRender
         /// <param name="drawingContext">Drawing context of the OutlineText control.</param>
         protected override void OnRender(DrawingContext drawingContext)
         {
+            // the black outline
             Brush Stroke = new SolidColorBrush(Color.FromRgb(0x00, 0x00, 0x00));
             Brush Fill = new SolidColorBrush(Color.FromRgb(0xff, 0xff, 0xff));
-            double StrokeThickness = 20.0;
-            // Draw the black outline based on the properties that are set.
+            double StrokeThickness = 22.0;
             drawingContext.DrawGeometry(Fill, new System.Windows.Media.Pen(Stroke, StrokeThickness), _textGeometry);
-            StrokeThickness /= 4.0;
+            // the red outline
+            StrokeThickness /= 3.0; // half on each side
             Stroke = new SolidColorBrush(Color.FromRgb(0xff, 0x00, 0x00));
-            // Draw the red outline based on the properties that are set.
             drawingContext.DrawGeometry(Fill, new System.Windows.Media.Pen(Stroke, StrokeThickness), _textGeometry);
         }
     }
